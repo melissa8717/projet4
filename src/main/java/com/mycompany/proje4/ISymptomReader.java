6d741e9eaafa4cd91cda28dd71ccfcf553889ed0
melissa8717 <melgraph.design@gmail.com>
2023-01-25 16:28:03 +0100

    init project

---
+++ src/main/java/com/mycompany/proje4/ISymptomReader.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.proje4;
+
+import java.util.List;
+
+/**
+ * Anything that will read symptom data from a source
+ * The important part is, the return value from the operation, which is a list of strings,
+ * that may contain many duplications
+ * 
+ * The implementation does not need to order the list
+ * 
+ */
+public interface ISymptomReader {
+	/**
+	 * If no data is available, return an empty List
+	 * 
+	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
+	 */
+	List<String> GetSymptoms ();
+}
+
